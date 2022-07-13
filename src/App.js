2eeada4ecf12a7f461bd266658d1a2135887987b
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-07-13 15:42:56 +0530

    Add: sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import Submenu from "./components/Submenu/Submenu";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* Sidebar */}
+      <Sidebar />
       <Hero />
       <Submenu />
     </>
