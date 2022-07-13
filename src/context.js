7d2fe709ba5bf664575c3a17ecccb134838a7421
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-07-13 11:28:09 +0530

    set-up context

---
+++ src/context.js
@@ -0,0 +1,50 @@
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+const AppContext = React.createContext();
+
+const AppProvider = ({ props }) => {
+  const [isSidebar, setIsSidebar] = useState(false);
+  const [isSubMenu, setIsSubMenu] = useState(false);
+  const [page, setPage] = useState({ page: "", links: [] });
+  const [location, setLocation] = useState({});
+
+  const openSidebar = () => {
+    setIsSidebar(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebar(false);
+  };
+
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubMenu(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubMenu(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebar,
+        openSidebar,
+        closeSidebar,
+        isSubMenu,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {props}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
